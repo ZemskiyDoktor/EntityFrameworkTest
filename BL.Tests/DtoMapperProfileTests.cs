22dc6643d6107d582f45eeb0c39a9ab384ae6e72
ZemskiyDoktor <zemskiydoktor@gmail.com>
2018-03-20 16:46:03 +0100

    add extention method

--- BL.Tests/DtoMapperProfileTests.cs
+++ BL.Tests/DtoMapperProfileTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using AutoFixture;
 using AutoFixture.NUnit3;
 using AutoMapper;
@@ -28,15 +30,27 @@ namespace BL.Tests
         [Test, AutoEfMoqData]
         public void TestGetAll([Frozen]Mock<DbSet<User>> users, [Frozen]Mock<LibraryContext> context, List<User> lusers)
         {
-            var qusers = lusers.AsQueryable();
-            users.As<IQueryable<User>>().Setup(m => m.Provider).Returns(qusers.Provider);
-            users.As<IQueryable<User>>().Setup(m => m.Expression).Returns(qusers.Expression);
-            users.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(qusers.ElementType);
-            users.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(() => qusers.GetEnumerator());
+            lusers[2].Name = "Ivan";
+            
+            context.Setup(users, lusers, c => c.Users);
+            
+            var user = context.Object.Users.FirstOrDefault(x => x.Name == "Ivan");
+            //var user = context.Object.Users.FirstOrDefault(x => x.Name == lusers[0].Name);
+            Assert.IsNotNull(user);
+        }
+    }
 
-            context.Setup(c => c.Users).Returns(users.Object);
+    internal static class DataTestExtentions
+    {
+        public static void Setup<TContext, T>(this Mock<TContext> context, Mock<DbSet<T>> dtSet, List<T> items, Expression<Func<TContext, DbSet<T>>> act) where T : class where TContext : class
+        {
+            var qitems = items.AsQueryable();
+            dtSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(qitems.Provider);
+            dtSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(qitems.Expression);
+            dtSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(qitems.ElementType);
+            dtSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => qitems.GetEnumerator());
 
-            var user = context.Object.Users.FirstOrDefault(x => x.Name == lusers[0].Name);
+            context.Setup(act).Returns(dtSet.Object);
         }
     }
 }
